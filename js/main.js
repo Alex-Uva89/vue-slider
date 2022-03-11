b4d20b7a1c3f98a7033cde4a859cdb1fbc3d5859
ALESSANDROUVA <a.uva1988@gmail.com>
2022-03-11 15:37:15 +0100

    1st push

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const app = new Vue({
+	el: '#container',
+	data: {
+        arrSlides: [
+            {
+                title:'Svezia',
+                image:'img/01.jpg',
+                txt:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+            },
+            {
+                title:'Svizzera',
+                image:'img/02.jpg',
+                txt:'Lorem ipsum',
+            },
+            {
+                title:'Gran Bretagna',
+                image:'img/03.jpg',
+                txt:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                title:'Germania',
+                image:'img/04.jpg',
+                txt:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+            {
+                title:'Paradice',
+                image:'img/05.jpg',
+                txt:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            },
+        ]
+    }
+		
+});
+
